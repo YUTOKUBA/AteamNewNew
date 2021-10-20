683a447d0105ec8798a8ec08f37dca9f1a1029a3
YUTOKUBA <yuuto-kuba-20h@kbc.stu.ac.jp>
2021-10-20 15:42:31 +0900

    特に追加無し

--- AteamNewNew/Assets/Script/camera.cs
+++ AteamNewNew/Assets/Script/camera.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class camera : MonoBehaviour
 {
     public GameObject ball;   //追いかけるターゲット
-    private Vector3 offset;     //カメラとターゲットの座標の差分
+    private Vector3 offset;   //カメラとターゲットの座標の差分
 
     void Start()
     {
@@ -14,7 +14,10 @@ public class camera : MonoBehaviour
 
     void Update()
     {
-        transform.position = ball.transform.position + offset;    //ターゲットの座標に、差分を追加した座標にカメラを移動させる
+        if (Mathf.Abs(ball.transform.position.x) >= 0.15f || Mathf.Abs(ball.transform.position.z) >= 0.15f)
+        //if(transform.position - ball.transform.position = 0.15f)
+        {
+            transform.position = ball.transform.position + offset; //ターゲットの座標に、差分を追加した座標にカメラを移動させ
+        }
     }
-
 }
