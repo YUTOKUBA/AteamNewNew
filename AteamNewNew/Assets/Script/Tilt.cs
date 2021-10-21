93c832b1af8dfe923571742f4e2cc7af7c10e5a4
KBC12006118\kbc12006118 <koudai-arashiro-20h@stu.kbc.ac.jp>
2021-10-21 15:18:48 +0900

    床のスクリプトを少し変更

--- AteamNewNew/Assets/Script/Tilt.cs
+++ AteamNewNew/Assets/Script/Tilt.cs
@@ -1,25 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 public class Tilt : MonoBehaviour
 {
-    Transform target;
-    float speed = 90f;
-    float back_speed = 60f;
+    private Transform target;
+    public float speed = 90f;
+    public float back_speed = 60f;
     bool keyon = false;
+    public float time;
+    public Text TimeText;
+
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.Find("Cube").transform;
+        time = 0.0f;
     }
     // Update is called once per frame
     void Update()
     {
+
         float step = speed * Time.deltaTime;
         float back_step = back_speed * Time.deltaTime;
         if (Input.GetAxisRaw("L_Stick_Y") == -1)
         {
             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(30f, 0, 0), step);
+
+            time += Time.deltaTime;
+
+            TimeText.text = time.ToString("F2");
         }
         else
         {
