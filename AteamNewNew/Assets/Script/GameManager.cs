372198ed969fc34e6fa39f28f16107a5cedba5ed
KBC2KA04\Owner <ren-ooishi-20h@stu.kbc.ac.jp>
2021-11-01 12:13:40 +0900

    GameManager内を見やすく変更 Start画面を追加予定

--- AteamNewNew/Assets/Script/GameManager.cs
+++ AteamNewNew/Assets/Script/GameManager.cs
@@ -6,24 +6,26 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject pauseUI;
+    //スタート画面
+    [SerializeField] private GameObject start_UI;    //スタート画面
+    bool Start_flg;                 //スタートフラグ
 
-    public RectTransform pauseCursor;
-
+    //ポーズ画面
+    [SerializeField]private GameObject pauseUI; //ポーズ画面
+    public RectTransform pauseCursor;           //カーソル
     int Menu_Num = 0;   //メニュー選択時の番号　0:リスタート 1:タイトル 2:終了
-
     bool Push_Flg = false; //連続入力防止用スイッチ
 
-    GameObject Ball;
-    coinkesu var;
+    //スコア表示
+    GameObject Ball;    //スコアが入っているオブジェクト
+    coinkesu var;       //空箱
+    public GameObject Score;    //表示用
 
-    public GameObject Score;
-
-    [SerializeField]
-    private GameObject clear_UI;    //クリア画面
+    //クリア
+    [SerializeField]private GameObject clear_UI;    //クリア画面
     bool Clear_flg;                 //クリアフラグ
 
+    //アニメーション
     [SerializeField]Animator Clear_animator;
     [SerializeField]Animator Result_animator;
 
@@ -34,8 +36,11 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        //スタート条件の初期化
+        Start_flg = true;
         //クリア条件の初期化
         Clear_flg = false;
+
         //表示設定
         pauseUI.SetActive(false);
         clear_UI.SetActive(false);
@@ -45,47 +50,63 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
+        //スタート時のアニメーション表示
+        if (Start_flg == true)
+        {
+            Game_Start();
+            Start_flg = false;
+        }
+        Pause_Controller();
+
         //スコアの取得と表示
         Ball = GameObject.Find("Ball");
         var = Ball.GetComponent<coinkesu>();
         Text score = Score.GetComponent<Text>();
         score.text = var.coin + " / 12";
+        
+        //ポーズの表示
+        if (Input.GetButtonDown("Pause") && Clear_flg != true)
+        {
+            Game_Pause();
+        }
+        
+        //クリア画面の表示
+        if (var.coin == 12)
+        {
+            Game_Clear();
+        }
+    }
 
+    void Game_Start()
+    {
+
+    }
+
+    void Game_Pause()
+    {
+        //　ポーズUIのアクティブ、非アクティブを切り替え
+        pauseUI.SetActive(!pauseUI.activeSelf);
+
+        //　ポーズUIが表示されてる時は停止
+        if (pauseUI.activeSelf)
+        {
+            Time.timeScale = 0f;
+            //　ポーズUIが表示されてなければ通常通り進行
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+        
+    }
+
+    void Pause_Controller()
+    {
         //Lスティックの入力
         float lsh = Input.GetAxis("L_stick_h");
         float lsv = Input.GetAxis("L_stick_v");
 
-        //デバッグ用
-        if (debug_mode != false)
-        {
-            x_object.SetActive(true);
-            y_object.SetActive(true);
-            Text x_text = x_object.GetComponent<Text>();
-            Text y_text = y_object.GetComponent<Text>();
-            x_text.text = "X = " + lsv;
-            y_text.text = "Y = " + lsh;
-        }
-
-        //ポーズの表示
-        if (Input.GetButtonDown("Pause") && Clear_flg != true)
-        {
-            //　ポーズUIのアクティブ、非アクティブを切り替え
-            pauseUI.SetActive(!pauseUI.activeSelf);
-
-            //　ポーズUIが表示されてる時は停止
-            if (pauseUI.activeSelf)
-            {
-                Time.timeScale = 0f;
-                //　ポーズUIが表示されてなければ通常通り進行
-            }
-            else
-            {
-                Time.timeScale = 1f;
-            }
-
-        }
-
-        //コントローラーの操作
+        //カーソルのの操作
         if (pauseUI.activeSelf)
         {
             if (lsv == 1)
@@ -159,23 +180,34 @@ public class GameManager : MonoBehaviour
             }
         }
 
+        //デバッグ用
+        if (debug_mode != false)
+        {
+            x_object.SetActive(true);
+            y_object.SetActive(true);
+            Text x_text = x_object.GetComponent<Text>();
+            Text y_text = y_object.GetComponent<Text>();
+            x_text.text = "X = " + lsv;
+            y_text.text = "Y = " + lsh;
+        }
 
-        //クリア画面の表示
-        if (var.coin == 12)
-        {        
-            clear_UI.SetActive(true);   //クリア画面の表示
-            Clear_flg = true;           //クリアフラグをON
-            Time.timeScale = 0f;        //ゲームを停止
-            Clear_animator.SetBool("IsCenter",true);   //アニメーションの表示
-            Result_animator.SetTrigger("IsZoom");      //
+    }
 
-            if (Input.GetButtonDown("A"))
-            {
-                Clear_animator.SetBool("IsCenter", false);//アニメーションの非表示
-                Time.timeScale = 1f;
-                Clear_flg = false;
-                Application.LoadLevel("Game");
-            }
+    void Game_Clear()
+    {
+
+        clear_UI.SetActive(true);   //クリア画面の表示
+        Clear_flg = true;           //クリアフラグをON
+        Time.timeScale = 0f;        //ゲームを停止
+        Clear_animator.SetBool("IsCenter", true);   //アニメーションの表示
+        Result_animator.SetTrigger("IsZoom");      //
+
+        if (Input.GetButtonDown("A"))
+        {
+            Clear_animator.SetBool("IsCenter", false);//アニメーションの非表示
+            Time.timeScale = 1f;
+            Clear_flg = false;
+            Application.LoadLevel("Game");
         }
     }
 }
