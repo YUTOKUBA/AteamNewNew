b7034177a6e5cf7d2bd240115543af11f8ed61e7
KBC12006064\kbc12006064 <ryuugo-higa-20h@stu.kbc.ac.jp>
2021-11-02 09:56:52 +0900

    再生時間を付けている途中

---
+++ AteamNewNew/Assets/Script/AudioFrame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioFrame : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            audioSource.Stop();
+            audioSource.Play();
+        }
+        //Debug.Log(audioSource.time);
+    }
+}
--- AteamNewNew/Assets/Script/PlayerAudio.cs
+++ AteamNewNew/Assets/Script/PlayerAudio.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAudio : MonoBehaviour {
+    AudioSource audioSource;
 
     public AudioClip sound;
-
-    void OnCollisionEnter(Collision collision) {
-        AudioSource.PlayClipAtPoint(sound, transform.position);
-    }
+    
+     void OnCollisionEnter(Collision collision) {
+       AudioSource.PlayClipAtPoint(sound, transform.position);
+     }
 }
