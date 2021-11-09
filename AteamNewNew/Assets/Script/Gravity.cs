ad03d3af55777e38e44a2d37b7c6ef25d1540a67
MSI\Dai06 <daisuke-zayasu-20h@stu.kbc.ac.jp>
2021-11-09 09:33:09 +0900

    ボール落下のみ（スクリプト）

--- AteamNewNew/Assets/Script/Gravity.cs
+++ AteamNewNew/Assets/Script/Gravity.cs
@@ -1,27 +1,71 @@
-﻿using System.Collections.Generic;
-using System.Collections;
-using UnityEngine;
+﻿//using System.Collections.Generic;
+//using System.Collections;
+//using UnityEngine;
 
+
+//public class Gravity : MonoBehaviour
+//{
+//    [SerializeField] private Vector3 localGravity;
+//    private Rigidbody rb;
+//    public float speed = 3;
+
+//    private void Start()
+//    {
+//        rb = this.GetComponent<Rigidbody>();
+//        rb.useGravity = false;          //最初にrigidBodyの重力を使わなくする
+//    }
+
+//    private void FixedUpdate()
+//    {
+//        SetLocalGravity();              //重力をAddForceでかけるメソッドを呼ぶ
+//    }
+
+//    private void SetLocalGravity()
+//    {
+//        rb.AddForce(localGravity * speed, ForceMode.Acceleration);      // 力を加える
+//    }
+//}
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
 
 public class Gravity : MonoBehaviour
 {
-    [SerializeField] private Vector3 localGravity;
-    private Rigidbody rb;
-    public float speed = 3;
+    [SerializeField, Tooltip("重力値")]
+    float pyhs = -1f;
+    [SerializeField, Tooltip("最大加速度")] float maxVelocity = 10;
+    public Vector3 velocity;
 
-    private void Start()
+    //地面に着いたかどうか
+    public bool isdodai { get { return transform.position.y <= 0; } }
+
+    void Update()
     {
-        rb = this.GetComponent<Rigidbody>();
-        rb.useGravity = false;          //最初にrigidBodyの重力を使わなくする
+        HitCheck();
+
+        //空中の時重力をかける
+        if (velocity.y > 0 || transform.position.y != 0)
+        {
+            //最大加速度以下なら移動量加算
+            if (Mathf.Abs(velocity.y) <= maxVelocity)
+                velocity.y += pyhs;
+
+            if (transform.position.y < 0)
+            {
+                transform.position = new Vector3(transform.position.x, 0, 0);
+                velocity.y = 0;
+            }
+
+        }
+
+        //移動
+        transform.position += velocity * Time.deltaTime;
     }
 
-    private void FixedUpdate()
+    public void AddForce(Vector3 alpha)
     {
-        SetLocalGravity();              //重力をAddForceでかけるメソッドを呼ぶ
-    }
-
-    private void SetLocalGravity()
-    {
-        rb.AddForce(localGravity * speed, ForceMode.Acceleration);      // 力を加える
+        velocity += alpha;
     }
 }
