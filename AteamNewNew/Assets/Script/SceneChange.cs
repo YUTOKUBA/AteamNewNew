bda4407bc6f319dc14a63149fff694ed4bb589b1
OoishiRen <ren-ooishi-20h@stu.kbc.ac.jp>
2021-11-18 13:42:51 +0900

    セレクト画面ｗ

--- AteamNewNew/Assets/Script/SceneChange.cs
+++ AteamNewNew/Assets/Script/SceneChange.cs
@@ -6,9 +6,14 @@ using UnityEngine.SceneManagement;
 public class SceneChange : MonoBehaviour
 {
     [SerializeField] private AudioSource SE_Audio;   //SE用のオーディオソース
+    [SerializeField] private AudioSource BGM_Audio;   //SE用のオーディオソース
+    public bool IsFade;
+    public double FadeOutSeconds = 1.0;
+    bool IsFadeOut = false;
+    double FadeDeltaTime = 0;
     void Start()
     {
-        
+        FadeDeltaTime = 0;
     }
 
     void Update()
@@ -16,7 +21,21 @@ public class SceneChange : MonoBehaviour
          if (Input.anyKeyDown)
         {
             SE_Audio.Play();
-            SceneManager.LoadSceneAsync("Game");
+            IsFadeOut = true;
+        }
+        if (IsFadeOut)
+        {
+            FadeDeltaTime += Time.deltaTime;
+            if (FadeDeltaTime == FadeOutSeconds)
+            {
+                FadeDeltaTime = FadeOutSeconds;
+                IsFadeOut = false;
+            }
+            BGM_Audio.volume = (float)(1.0 - FadeDeltaTime / FadeOutSeconds);
+        }
+        if (BGM_Audio.volume == 0.0f)
+        {
+            SceneManager.LoadSceneAsync("Stage_Select");
         }
     }
 }
---
+++ AteamNewNew/Assets/Script/Stage_Select.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stage_Select : MonoBehaviour
+{
+    [SerializeField] private AudioSource Select_Audio;   //SE用のオーディオソース
+    bool Push_Flg = false; //連続入力防止用スイッチ
+
+    [SerializeField] private RectTransform Center;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float lsh = Input.GetAxis("L_stick_h");
+        float lsv = Input.GetAxis("L_stick_v");
+        if (lsh == 1)
+        {
+            if (Push_Flg == false)
+            {
+                Push_Flg = true;
+                Center.transform.Rotate(new Vector3(0,+90,0));
+            }
+        }
+        else if (lsh == -1)
+        {
+            if (Push_Flg == false)
+            {
+                Push_Flg = true;
+                Center.transform.Rotate(new Vector3(0, -90, 0));
+            }
+        }
+        else
+        {
+            Push_Flg = false;
+        }
+        if (Input.GetButtonDown("A"))
+        {
+            Select_Audio.Play();
+            SceneManager.LoadSceneAsync("Game");
+        }
+    }
+}
