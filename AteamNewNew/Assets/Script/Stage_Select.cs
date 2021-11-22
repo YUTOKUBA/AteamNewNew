b070755cc32af724e2fec3618c174a4a4bde9bf6
OoishiRen <ren-ooishi-20h@stu.kbc.ac.jp>
2021-11-22 14:37:32 +0900

    ステージセレクトを変更

--- AteamNewNew/Assets/Script/Stage_Select.cs
+++ AteamNewNew/Assets/Script/Stage_Select.cs
@@ -8,10 +8,19 @@ public class Stage_Select : MonoBehaviour
     [SerializeField] private AudioSource Select_Audio;   //SE用のオーディオソース
     bool Push_Flg = false; //連続入力防止用スイッチ
 
+    [SerializeField] private AudioSource BGM_Audio;   //SE用のオーディオソース
+
     [SerializeField] private RectTransform Center;
+
+    public bool IsFade;
+    public double FadeOutSeconds = 1.0;
+    bool IsFadeOut = false;
+    double FadeDeltaTime = 0;
+
+    bool IsRotate = false;
     void Start()
     {
-
+        FadeDeltaTime = 0;
     }
 
     void Update()
@@ -23,7 +32,7 @@ public class Stage_Select : MonoBehaviour
             if (Push_Flg == false)
             {
                 Push_Flg = true;
-                Center.transform.Rotate(new Vector3(0,+90,0));
+                IsRotate = true;
             }
         }
         else if (lsh == -1)
@@ -41,6 +50,26 @@ public class Stage_Select : MonoBehaviour
         if (Input.GetButtonDown("A"))
         {
             Select_Audio.Play();
+            IsFadeOut = true;
+        }
+
+        if (IsRotate)
+        {
+            Center.transform.Rotate(new Vector3(0, 10.0f * Time.deltaTime, 0));
+        }
+
+        if (IsFadeOut)
+        {
+            FadeDeltaTime += Time.deltaTime;
+            if (FadeDeltaTime == FadeOutSeconds)
+            {
+                FadeDeltaTime = FadeOutSeconds;
+                IsFadeOut = false;
+            }
+            BGM_Audio.volume = (float)(1.0 - FadeDeltaTime / FadeOutSeconds);
+        }
+        if (BGM_Audio.volume == 0.0f)
+        {
             SceneManager.LoadSceneAsync("Game");
         }
     }
