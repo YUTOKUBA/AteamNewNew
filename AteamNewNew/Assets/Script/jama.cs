bdfd4912d361f07d6fe2ce5efc528b96738569a8
YUTOKUBA <yuuto-kuba-20h@kbc.stu.ac.jp>
2021-11-24 09:43:41 +0900

    ステージ選択画面、ボールの色変化を調整

--- AteamNewNew/Assets/Script/jama.cs
+++ AteamNewNew/Assets/Script/jama.cs
@@ -8,18 +8,36 @@ public class jama: MonoBehaviour
     {
         gameObject.GetComponent<Rigidbody>().Resume(gameObject);
     }
-        
+
+    //ボールの色を帰る関数
+    void ColorChange()
+    {
+         GetComponent<Renderer>().material.color = Color.red;
+        Invoke("ColorWhite",1.5f);
+    }
+
+    //ボールの色を白色にする
+    void ColorWhite()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         // もし衝突した相手オブジェクトの名前が"jama"ならば
         if (collision.gameObject.tag == "jama")
         {
             gameObject.GetComponent<Rigidbody>().Pause(gameObject);
+
             // 衝突した相手オブジェクトを削除する
             Destroy(collision.gameObject);
+
+            //ボールの色を変える
+            ColorChange();
+
             //動きを再開
-            Invoke("restart", 1.5f);  
+            Invoke("restart", 1.5f);
         }
-        
+
     }
 }
