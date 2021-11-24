e5fa34397387dba513481e791613791a56839262
OoishiRen <ren-ooishi-20h@stu.kbc.ac.jp>
2021-11-24 14:25:34 +0900

    元通り

---
+++ AteamNewNew/Assets/Script/Fall_Maru.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fall_Maru : MonoBehaviour
+{
+    void Update()
+    {
+        Rigidbody rb = this.GetComponent<Rigidbody>();
+        Vector3 tmp = GameObject.Find("Ball").transform.position;
+        if (this.transform.position.y < -10)
+        {
+            rb.velocity = Vector3.zero;
+            GameObject.Find("Ball").transform.position = new Vector3(0, 1.5f, 2f);
+        }
+    }
+}
+
+
