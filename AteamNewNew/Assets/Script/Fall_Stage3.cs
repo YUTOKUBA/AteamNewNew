f6d08341af65a5cf311c123535bdab3be9b704c0
YUTOKUBA <yuuto-kuba-20h@kbc.stu.ac.jp>
2021-11-24 14:10:28 +0900

    respawn

---
+++ AteamNewNew/Assets/Script/Fall_Stage3.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fall_Stage3: MonoBehaviour
+{
+    void Update()
+    {
+        Rigidbody rb = this.GetComponent<Rigidbody>();
+        Vector3 tmp = GameObject.Find("Ball").transform.position;
+        if (this.transform.position.y < -10)
+        {
+            rb.velocity = Vector3.zero;
+            GameObject.Find("Ball").transform.position = new Vector3(0, 0.8f, -0.4f);
+        }
+    }
+}
+
+
