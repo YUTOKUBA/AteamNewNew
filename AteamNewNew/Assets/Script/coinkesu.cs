1bb25f132091455e546388b15f63f2c1dfd5a876
OoishiRen <ren-ooishi-20h@stu.kbc.ac.jp>
2021-11-30 16:18:46 +0900

    いろいろ修正

--- AteamNewNew/Assets/Script/coinkesu.cs
+++ AteamNewNew/Assets/Script/coinkesu.cs
@@ -17,7 +17,6 @@ public class coinkesu : MonoBehaviour
         // もし衝突した相手オブジェクトの名前が"Cube"ならば
         if (collision.gameObject.tag == "cube")
         {
-            Debug.Log("ok");
             coin++;
             // 衝突した相手オブジェクトを削除する
             Debug.Log("反応している。");
