89e733595d6cdad52f747dba9464aac1999fa504
OoishiRen <ren-ooishi-20h@stu.kbc.ac.jp>
2021-12-02 15:23:26 +0900

    リトライ画面のカーソルを変更。 ステージ2の壁抜けを修正しました。

---
+++ AteamNewNew/Assets/Script/Parent_2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parent_2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void kaijo()
+    {
+        this.gameObject.transform.parent = null;    //親子関係を解除
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")     //もしタグがjamaのオブジェクトに衝突したら
+        {
+            transform.parent = GameObject.Find("DODAI").transform;  //DODAIの子オブジェクトになる
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "jama")     //もしタグがjamaのオブジェクトに衝突したら
+        {
+            transform.parent = GameObject.Find("DODAI").transform;  //DODAIの子オブジェクトになる
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")     //もしタグがjamaのオブジェクトに衝突したら
+        {
+            kaijo();
+        }
+    }
+}
