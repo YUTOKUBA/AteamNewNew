265e125ad0896ff2a1399a2981670da16d84ba88
OoishiRen <ren-ooishi-20h@stu.kbc.ac.jp>
2021-12-02 11:42:16 +0900

    スクリプトを変更しました。

--- AteamNewNew/Assets/Script/MoveDodai/MoveDodaiTate2.cs
+++ AteamNewNew/Assets/Script/MoveDodai/MoveDodaiTate2.cs
@@ -6,17 +6,29 @@ public class MoveDodaiTate2 : MonoBehaviour
 {
     public int counter = 0;
     public float move = -0.002f;
+    public GameObject GameManager;
+
+    void Start()
+    {
+        GameManager = GameObject.Find("GameManager");
+    }
+
 
     void Update()
     {
-        Vector3 pos = new Vector3(0, 0, move);
-        transform.Translate(pos);
-        counter++;
-
-        if (counter == 50)
+        if (GameManager.GetComponent<GameManager>().pause_flg == false &&
+            GameManager.GetComponent<GameManager>().Start_flg == false &&
+            GameManager.GetComponent<GameManager>().Clear_flg == false)
         {
-            counter = 0;
-            move *= -1;
+            Vector3 pos = new Vector3(0, 0, move);
+            transform.Translate(pos);
+            counter++;
+
+            if (counter == 50)
+            {
+                counter = 0;
+                move *= -1;
+            }
         }
     }
 }
--- AteamNewNew/Assets/Script/MoveWall/MoveWallTate.cs
+++ AteamNewNew/Assets/Script/MoveWall/MoveWallTate.cs
@@ -6,17 +6,27 @@ public class MoveWallTate : MonoBehaviour
 {
     public int counter = 0;
     public float move = 0.01f;
+    public GameObject GameManager;
+
+    void Start()
+    {
+        GameManager = GameObject.Find("GameManager");
+    }
 
     void Update()
     {
-        Vector3 pos = new Vector3(0, 0, move);
-        transform.Translate(pos);
-        counter++;
+        if (GameManager.GetComponent<GameManager>().pause_flg == false &&
+            GameManager.GetComponent<GameManager>().Start_flg == false &&
+            GameManager.GetComponent<GameManager>().Clear_flg == false) {
+            Vector3 pos = new Vector3(0, 0, move);
+            transform.Translate(pos);
+            counter++;
 
-        if (counter == 150)
-        {
-            counter = 0;
-            move *= -1;
+            if (counter == 150)
+            {
+                counter = 0;
+                move *= -1;
+            }
         }
     }
 }
